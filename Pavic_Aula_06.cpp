a1bf1c1e0fd0a00d2b390455d936f96de43b1a3f
ThuannePaixao <thuannepaixao@gmail.com>
2022-10-13 19:37:51 -0300

    Adicionar arquivos de projeto.

---
+++ Pavic_Aula_06.cpp
@@ -0,0 +1,30 @@
+/**
+ * Two shoppers adding items to a shared notepad
+ */
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+unsigned int garlic_count = 0;
+std::mutex pencil;
+
+void shopper() {
+    //pencil.lock();
+    for (int i = 0; i < 5; i++) {
+        printf("Shopper %d is thinking...\n", std::this_thread::get_id());
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        pencil.lock();
+        garlic_count++;
+        pencil.unlock();
+    }
+}
+
+int main() {
+    std::thread barron(shopper);
+    std::thread olivia(shopper);
+    std::thread antonio(shopper);
+    barron.join();
+    olivia.join();
+    antonio.join();
+    printf("We should buy %u garlic.\n", garlic_count);
+}
